123c3ba5120847fc29909d7e2c253845ca48640b
theptrk <patrick.tran06@gmail.com>
2014-08-21 15:34:14 -0700

    Added modified Linked List

--- index.js
+++ index.js
@@ -1,26 +1,5 @@
-var Link = function(data){
-  this.data = data;
-  this.next = null;
-};
-Link.prototype.each = function(cb) {
-  cb(this);
-
-  if (this.next !== null) {
-    this.next.each(cb);
-  }
-};
-
-// `tail` recurses until it finds the tail and returns the result of a 
-// invoking a passed in callback on the tail.
-Link.prototype.tail = function(cb){
-  
-  if ( this.next !== null ) {
-    return this.next.tail(cb);
-  } else {
-    return cb === undefined ? this: cb(this);
-  }
-};
+var mLink = require('./lib/mLink');
 
 module.exports = {
-  Link: Link
+  mLink: mLink
 };
